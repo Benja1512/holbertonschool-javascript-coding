78d4c1c92f835eba6527bc0bb4ae8ed8e7dd8f30
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-16 17:19:08 -0500

    fixed: code

--- 0-readme.js
+++ 0-readme.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+#!/usr/bin/node
+// Reads and prints the content of a file.
+const fs = require ('fs');
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf-8', (err, data) => {
