32f5172044dbff8d4b37804c4ce2e450f8b344a6
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-16 17:40:53 -0500

    fixed: code

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// Writes a string to a file.
-const fs = require('fs');
-const fileName = process.argv[2];
-const fileText = process.argv[3];
 
-fs.writeFile(fileName, fileText, 'utf-8', (err) => {
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
 });
