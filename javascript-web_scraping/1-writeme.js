6c6cf52cc3b033efa221c2d19db549f23972a856
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-18 08:30:11 -0500

    fixeD: code eslint

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
 });
