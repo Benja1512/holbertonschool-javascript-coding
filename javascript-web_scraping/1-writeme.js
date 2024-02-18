8babef206f3ed7245e9ae5a74d50f6eee7257a76
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-18 16:25:20 -0500

    feat: code fixed

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
-  if (err) {
-    console.log(err);
-  }
+if (process.argv.length < 4) process.exit(1);
+const fp = process.argv[2];
+const strWrite = process.argv[3];
+fs.writeFile(fp, strWrite, 'utf-8', (err) => {
+  if (err) console.log(err);
 });
